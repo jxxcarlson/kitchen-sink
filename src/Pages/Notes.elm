303b715a2f9bc508dbfb6266a3dd2550683bcd57
James Carlson <jxxcarlson@gmail.com>
2023-12-29 14:02:30 -0500

    Remove cowboy.mp3

---
+++ src/Pages/Notes.elm
@@ -0,0 +1,45 @@
+module Pages.Notes exposing (view)
+
+import Element exposing (Element)
+import MarkdownThemed
+import Types exposing (..)
+
+
+view : LoadedModel -> Element msg
+view model =
+    """
+#  Notes
+
+The Kitchen Sink Template is a template for creating Lamdera apps
+which has a lot of features already set up for you.
+
+## Adding a new page
+
+Suppose that you want to add a new page for notes to your app.
+You must first set up and configure a the route for the page.
+Define a new variant `Notes` for the type
+`Route` in `Routes.elm`.
+Then follow the compiler errors to update the functions
+`decode : Url -> Route`
+and
+`encode : Route -> String` in that module.
+
+Next, add a new module `Pages.Notes`, put some
+draft content in it, import it into module
+`Frontend`, and add an entry in the
+function `Frontend.update` to handle the
+new route.
+
+If you want to add a link to the new page in the
+footer, add something like the below to
+`Pages.Parts.footer`:
+
+```elm
+Element.link [] { url = Route.encode Notes, label = Element.text "Notes"
+```
+
+Another possibility is to add a link to `Pages.parts.header`:
+
+
+"""
+        |> MarkdownThemed.renderFull
