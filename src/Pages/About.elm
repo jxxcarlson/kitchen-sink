3e947dc6ad6c3ce89f61edd5be2610bd3cc850a5
James Carlson <jxxcarlson@gmail.com>
2023-12-29 14:09:34 -0500

    - Modify MarkdownThemed - Removed unused code - Add new lage Notes - Move header into Pages.Parts and make it look like footer - Add Notes as variant of type Route, update decode and encode in module Route

--- src/Pages/About.elm
+++ src/Pages/About.elm
@@ -1,9 +1,28 @@
 module Pages.About exposing (..)
 
 import Element exposing (Element)
+import MarkdownThemed
 import Types exposing (..)
 
 
-view : LoadedModel -> Element FrontendMsg
+view : LoadedModel -> Element msg
 view model =
-    Element.text "About"
+    """
+# About the Template
+
+The kitchen sink app is a starting point for your
+Lamdera projects.  It comes with a number of
+built-in features:
+
+- Page routing
+- Stripe
+- Ports: an example to ...
+- Authentication
+- RPC: ...
+- Markdown: ...
+
+For additional information, see the **Notes** tab.
+
+
+"""
+        |> MarkdownThemed.renderFull
