28cf6b77eeab38c199f97ba82242eadf5164ab08
James Carlson <jxxcarlson@gmail.com>
2024-01-01 17:37:07 -0500

    Update Featuers pages

--- src/Pages/Purchase.elm
+++ src/Pages/Purchase.elm
@@ -10,7 +10,8 @@ import Types
 view : Types.LoadedModel -> Element.Element Types.FrontendMsg
 view model =
     Element.column [ Element.paddingEach { top = 50, bottom = 9, left = 0, right = 0 } ]
-        [ Element.el [ Font.bold, Font.size 24 ] (Element.text "Purchase Item:")
+        [ Element.el [ Font.italic, Font.size 24 ] (Element.text "(( Oops!  A regression ... fox coming soon! ))")
+        , Element.el [ Font.bold, Font.size 24 ] (Element.text "Purchase Item:")
         , Stripe.View.productList model model.productInfoDict model.prices
         , case model.selectedProduct of
             Nothing ->
