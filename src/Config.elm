79a896575010687a19e8f10bc1e1d42318780231
James Carlson <jxxcarlson@gmail.com>
2024-04-27 18:35:42 -0400

    commit api key referenced via Env

--- src/Config.elm
+++ src/Config.elm
@@ -16,8 +16,7 @@ contactEmail =
 
 postmarkApiKey : Postmark.ApiKey
 postmarkApiKey =
-    -- Postmark.apiKey Env.postmarkApiKey
-    Postmark.apiKey "e4457a18-289d-4dd2-96f5-2ccf68dc2879"
+    Postmark.apiKey Env.postmarkApiKey
 
 
 postmarkNoReplyEmail : String
